458bac9a679543a5b7377d7b3faa416de342179e
Vinay Nikam <vinaynikam107@gmail.com>
2023-08-26 12:41:51 +0530

    implement copy text logic

--- app/src/main/java/com/example/ishakachinavka/Adapter/ShayariDataShowAdpter.kt
+++ app/src/main/java/com/example/ishakachinavka/Adapter/ShayariDataShowAdpter.kt
@@ -1,15 +1,19 @@
 package com.example.ishakachinavka.Adapter
 
+import android.R.attr.text
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ishakachinavka.Model.IntroScreenItem
+import com.example.ishakachinavka.Model.Shayari
 import com.example.ishakachinavka.databinding.ShowshayariDesignlayoutBinding
 
 
-class ShayariDataShowAdpter(var context: Context, var datalist:MutableList<String>): RecyclerView.Adapter<ShayariDataShowAdpter.Myviewholder>() {
+class ShayariDataShowAdpter(var context: Context, var datalist:MutableList<Shayari>): RecyclerView.Adapter<ShayariDataShowAdpter.Myviewholder>() {
 
     lateinit var binding: ShowshayariDesignlayoutBinding
 
@@ -31,12 +35,38 @@ class ShayariDataShowAdpter(var context: Context, var datalist:MutableList<Strin
     override fun onBindViewHolder(holder: Myviewholder, position: Int) {
 
         var datalisst=datalist[position]
+        holder.binding.shayariTextview.text=datalisst.text
 
-        holder.binding.shayariTextview.text=datalisst
-       /* holder.binding.textDate2.text=datalisst.date
-        holder.binding.textDay2.text=datalisst.day
-        holder.binding.textintime3.text=datalisst.intime
-        holder.binding.textOutTime2.text=datalisst.outtime*/
+
+        holder.binding.copyBtn.setOnClickListener {
+
+
+            val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+            val clip = ClipData.newPlainText("msg",datalist.get(position).text)
+
+            Log.e("shayarii", "onBindViewHolder: "+datalist.get(position).text)
+            assert(clipboard != null)
+            clipboard!!.setPrimaryClip(clip)
+            Toast.makeText(context, "Copied To Clipboard !", Toast.LENGTH_SHORT).show()
+
+
+
+
+        }
+
+        holder.binding.favBtn.setOnClickListener {
+            Toast.makeText(context, "fav", Toast.LENGTH_SHORT).show()
+
+
+        }
+        holder.binding.shareBtn.setOnClickListener {
+            Toast.makeText(context, "share", Toast.LENGTH_SHORT).show()
+
+
+
+
+
+        }
 
     }
 
---
+++ app/src/main/java/com/example/ishakachinavka/Model/Shayari.kt
@@ -0,0 +1,3 @@
+package com.example.ishakachinavka.Model
+
+data class Shayari(val text: String)
